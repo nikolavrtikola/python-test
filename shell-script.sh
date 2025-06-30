eebfa6db6627455e37dc884bc958e34e07f04430
kokismoki <zkoljo@yahoo.com>
2025-06-30 10:41:10 +0200

    Executing shell script

---
+++ shell-script.sh
@@ -0,0 +1,9 @@
+
+        echo "List files"
+        ls -l
+        cat src/math_operations.py  
+
+        sudo apt-get install cowsay -y
+        cowsay -f dragon "Run for cover I'm a dragon ....Rawrwas" >> dragon.txt
+        grep -i "dragon" dragon.txt
+        run: cat dragon.txt
